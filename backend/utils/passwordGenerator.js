9e40a3900a051158ceef6433e5a601f38c93864b
Suresh-Kannan-R-V <153901923+Suresh-Kannan-R-V@users.noreply.github.com>
2026-08-22 12:10:06 +0530

    Add temporary password generator function

---
+++ backend/utils/passwordGenerator.js
@@ -0,0 +1,10 @@
+function generateTemporaryPassword(length = 8) {
+  const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz23456789@#$';
+  let pass = '';
+  for (let i = 0; i < length; i++) {
+    pass += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return pass;
+}
+
+module.exports = { generateTemporaryPassword };
